cf71863c422674d17a0c96a785e1d41f03cc64ae
msa travel <derismaulana90@gmail.com>
2025-07-27 16:25:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-      const buttons = document.querySelectorAll(".wa-button");
-      buttons.forEach(btn => {
-        btn.addEventListener("click", function (e) {
-          e.preventDefault();
-          fetch("https://script.google.com/macros/s/AKfycbyvBlzGHcKHcTfaxd96KtgDBvHyKpasFmNTT-33tTy2zQVQbh_90BTtcqio-t4SrqYiqQ/exec")
-            .then(res => res.text())
-            .then(link => {
-              window.open(link, "_blank");
-            })
-            .catch(() => alert("Gagal membuka WhatsApp. Silakan coba lagi."));
-        });
-      });
-    });
-
 function sendToAppsScript() {
   const nama = document.getElementById("nama").value;
   const tanggal = document.getElementById("tanggal").value;
   const durasi = document.getElementById("durasi").value;
 
   const scriptURL = "https://script.google.com/macros/s/AKfycbxaT8_SJ0-xg0xnXsleZ1S56z6Q9GlOKI1B0_sf1OMtzhJwS52up7T9BQndfFg4QYZajg/exec";
-
   const url = `${scriptURL}?nama=${encodeURIComponent(nama)}&tanggal=${tanggal}&durasi=${durasi}`;
 
   window.open(url, "_blank");
-
   return false;
 }
 
