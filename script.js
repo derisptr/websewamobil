bd9c3c6e07e37035aeb5d1458c62f0e08ddde951
msa travel <derismaulana90@gmail.com>
2025-07-27 15:30:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,3 +33,11 @@ window.onscroll = function() {
       const btn = document.getElementById("toTopBtn");
       btn.style.display = window.scrollY > 300 ? "block" : "none";
     };
+
+  const container = document.getElementById('galeriContainer');
+  document.querySelector('.galeri-prev').onclick = () => {
+    container.scrollBy({ left: -300, behavior: 'smooth' });
+  };
+  document.querySelector('.galeri-next').onclick = () => {
+    container.scrollBy({ left: 300, behavior: 'smooth' });
+  };
